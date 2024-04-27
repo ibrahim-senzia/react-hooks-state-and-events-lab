b0e3bd740e895055113af5d949512f464bd1acd1
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-04-27 15:20:03 +0300

    state-lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [itemClass, setItemClass] = useState("");
+  const [btnClass, setBtnClass] = useState("");
+
+  function handleItemClass () {
+    if (itemClass === "") {
+      setItemClass("in-cart");
+      name = "Remove from Cart"
+    } else {
+      setItemClass("");
+      name = "Add to Cart"
+    }
+  }
+
+  function handleBtnClass () {
+    if (btnClass === "add") {
+      setBtnClass("remove");
+    } else {
+      setBtnClass("add");
+    }
+  }
+
+  const btnText = btnClass === "add" ? "Add to Cart" : "Remove From Cart"
+
   return (
-    <li className="">
+    <li className={itemClass} onClick={handleItemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={btnClass} onClick={handleBtnClass}>{btnText}</button>
     </li>
   );
 }
